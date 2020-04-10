048add883c4fe30a4b6cddc3a912c636fbd08d54
glaucoserrano <51676475+glaucoserrano@users.noreply.github.com>
2020-04-10 13:54:31 -0300

    express
    Curso expressa devmedia

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const path = require('path');
 const app = express();
 
 
+app.use(express.static("public"));
 app.get("/", function(req,res){
     res.sendfile(__dirname + "/public/page/index.html");
 });
-
+app.get("/sobre", function(req,res){
+    res.sendfile(__dirname + "/public/page/sobre.html");
+});
+app.get("/contato", function(req,res){
+    res.sendfile(__dirname + "/public/page/contato.html");
+});
 app.listen(8080, function(){
     console.log("executando");
 });
